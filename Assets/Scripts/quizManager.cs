ce675383f5fc3990c88976eb2f3fd4fd8a2b8d4c
Liana Mohd Farid <azlin.adliana@gmail.com>
2022-02-13 20:15:06 +0000

    attached quizDiff scene to quiz scene

--- Assets/Scripts/quizManager.cs
+++ Assets/Scripts/quizManager.cs
@@ -1,18 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class quizManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void goQuiz(){
+        SceneManager.LoadScene("quiz");
     }
 }
